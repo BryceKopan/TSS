cec211259e9983ee6886d6f6a99803a8c1f8b228
BryceKopan <BryceKopan@gmail.com>
2018-09-01 20:55:39 -0500

    Init Commit

---
+++ TwinStickShooter/Assets/Camera/TopDownCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float height = 10f;
+    [SerializeField]
+    private float distance = 20f;
+    [SerializeField]
+    private float angle = 45f;
+    [SerializeField]
+    private float smoothSpeed = 0.5f;
+
+    private Vector3 refVelocity;
+    // Use this for initialization
+    void Start()
+    {
+        HandleCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleCamera();
+    }
+
+    void HandleCamera()
+    {
+        if (!target)
+            return;
+
+        Vector3 worldPosition = (Vector3.forward * -distance) + (Vector3.up * height);
+        worldPosition = Quaternion.AngleAxis(angle, Vector3.up) * worldPosition;
+
+        Vector3 flatTargetPosition = target.position;
+        flatTargetPosition.y = 0f;
+
+        worldPosition = flatTargetPosition + worldPosition;
+
+        //transform.position = Vector3.SmoothDamp(transform.position, worldPosition, ref refVelocity, smoothSpeed);
+        transform.position = worldPosition;
+        transform.LookAt(target.position);
+    } 
+}
---
+++ TwinStickShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 10f;
+
+    private Vector3 movementDirection;
+    private Actions actions;
+
+    // Use this for initialization
+    void Start()
+    {
+        actions = gameObject.GetComponent<Actions>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleInput();
+
+        transform.position += movementDirection * movementSpeed * Time.deltaTime;
+
+        if (movementDirection != new Vector3(0, 0, 0))
+            actions.Walk();
+        else if (movementDirection == new Vector3(0, 0, 0))
+            actions.Idle();
+    }
+
+    void handleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movementDirection += Vector3.forward;
+            transform.LookAt(transform.position + movementDirection);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            movementDirection -= Vector3.forward;
+            transform.LookAt(transform.position + movementDirection);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            movementDirection += Vector3.left;
+            transform.LookAt(transform.position + movementDirection);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            movementDirection -= Vector3.left;
+            transform.LookAt(transform.position + movementDirection);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            movementDirection += Vector3.back;
+            transform.LookAt(transform.position + movementDirection);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            movementDirection -= Vector3.back;
+            transform.LookAt(transform.position + movementDirection);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            movementDirection += Vector3.right;
+            transform.LookAt(transform.position + movementDirection);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            movementDirection -= Vector3.right;
+            transform.LookAt(transform.position + movementDirection);
+        }
+    }
+}
